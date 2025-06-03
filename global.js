6e5770ee195133ea55c8a0c1df9644ea12e2cd94
Macintosh-Fan <toffeefur@icloud.com>
2025-06-03 10:14:41 -0700

    Fixed colour appearance for light/dark modes (with a bug)

--- global.js
+++ global.js
@@ -8,18 +8,21 @@ function darkModeToggle() {
     if (bgColor === BRIGHT) {
         bodyStyle.backgroundColor = DARK;
         bodyStyle.color = BRIGHT;
+        bodyStyle.filter = "100%";
     } else {
         bodyStyle.backgroundColor = BRIGHT;
         bodyStyle.color = DARK;
+        bodyStyle.filter = "75%";
     }
 }
 // i didnt want to change your function so i just added an event listner to call ur function
 window.addEventListener("DOMContentLoaded", function () {
     const toggle = document.getElementById("toggle_dark");
-    const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
+    const prefersLight = window.matchMedia(
+        "(prefers-color-scheme: light)",
+    ).matches;
     toggle.addEventListener("change", darkModeToggle);
-    if (prefersLight){
-        toggle.click()
+    if (prefersLight) {
+        toggle.click();
     }
-    
-})
+});
