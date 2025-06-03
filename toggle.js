2fa063f14cb62b1615ffa1b263599d0496014f1e
parsa222 <70150489+parsa222@users.noreply.github.com>
2025-06-03 13:50:12 -0700

    style ver 3.2

---
+++ toggle.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("toggle_dark");
+
+  function darkModeToggle(e) {
+    if (e.target.checked) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }
+  toggle.addEventListener("change", darkModeToggle);
+
+
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  if (prefersDark) {
+    toggle.click();
+  }
+})
